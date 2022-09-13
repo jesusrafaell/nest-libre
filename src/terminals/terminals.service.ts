7d5aab0dbbd40882134d29355f0ca33436d19c68
Jesús R. Hernandez Vera <jhernandez@tranred.com.ve>
2022-09-13 16:52:50 -0400

    termianls

--- src/terminals/terminals.service.ts
+++ src/terminals/terminals.service.ts
@@ -11,6 +11,10 @@ export class TerminalsService {
     comerCantPost: number,
   ): Promise<Resp> {
     try {
+      console.log(comerRif, comerCantPost);
+      //validar si exsite el comercio
+      //verificar el numero de afiliado del comercio
+      //
       const info = {
         message: 'Terminales',
       };
