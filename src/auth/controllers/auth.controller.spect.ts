eb9694510d4d323c82b8165a36d084a95db43d11
Jesús R. Hernandez Vera <jhernandez@tranred.com.ve>
2022-09-23 16:49:05 -0400

    test

---
+++ src/auth/controllers/auth.controller.spect.ts
@@ -0,0 +1,14 @@
+import { AuthService } from '../services/auth.service';
+import { AuthController } from './auth.controller';
+
+describe('CatsController', () => {
+  let authController: AuthController;
+  let authService: AuthService;
+
+  beforeEach(() => {
+    // authService = new AuthService();
+    // authController = new AuthController(authService);
+  });
+
+  describe('login', () => {});
+});
