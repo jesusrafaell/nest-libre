a2bc9dd44c18661bdb0dcc346d3406fafdef8f5f
Jesús R. Hernandez Vera <jhernandez@tranred.com.ve>
2022-10-31 17:27:51 -0400

    https

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
+//import https from 'https';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('key.pem'),
+    cert: fs.readFileSync('cert.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
+
   await app.listen(8000);
   console.log(`
     #       ### ######  ######  ####### ######     #     #####  ####### 
